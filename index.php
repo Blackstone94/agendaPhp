8cea7f2d100059e3d15424d5129ad5b306b37ef7
BlackStone94 <eroa94@gmail.com>
2020-10-22 13:12:41 -0500

    367 ok

--- index.php
+++ index.php
@@ -26,6 +26,53 @@
             </div>
     </form>
 </div>
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contactos">
+        <h2>Contactos</h2>
+        <input type="text" name="" id="buscar" class="buscador" placeholder="Buscar Contacto">
+        <p class="total-contactos"> <span>2 </span>Contactos</p>
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Telefono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    <tr>
+                        <td>Juan</td>
+                        <td>Udemy</td>
+                        <td>012356</td>
+                        <td>
+                            <a class="btn btn-editar" href="#"> 
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn btn-borrar">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Udemy</td>
+                        <td>012356</td>
+                        <td>
+                            <a class="btn btn-editar" href="#"> 
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn btn-borrar">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <?php
     include 'includes/layout/footer.php'; 
 ?>
